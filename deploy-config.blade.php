944d8325c4cfefcdc1ca74eb6fd51da0b1bf4181
Sheldon <sheldonreiff@gmail.com>
2019-08-18 17:27:25 -0400

    Update deploy-config.blade.php

--- deploy-config.blade.php
+++ deploy-config.blade.php
@@ -1,4 +1,4 @@
 container_commands:
     01-migration:
-        command: "sudo docker exec $(sudo docker ps -qf ancestor="{{ $app_image }}") php /var/www/html/artisan migrate"
+        command: "sudo docker exec $(sudo docker ps -qf ancestor={{ $app_image }}) php /var/www/html/artisan migrate"
         leader_only: true
