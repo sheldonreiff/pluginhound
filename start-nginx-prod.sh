44f5ce9ca0ffbd93e28da385569b8e58751f100d
Sheldon <sheldonreiff@gmail.com>
2019-08-26 20:47:46 -0400

    Update start-nginx-prod.sh

--- start-nginx-prod.sh
+++ start-nginx-prod.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -xe
 
-if [ ! -d /etc/letsencrypt/live ];
-then
-	certbot --debug --non-interactive --agree-tos --debug --nginx --email sheldonreiff@gmail.com --domains test1.pluginhound.com --keep-until-expiring
-fi
+# if [ ! -d /etc/letsencrypt/live ];
+# then
+# 	certbot --debug --non-interactive --agree-tos --debug --nginx --email sheldonreiff@gmail.com --domains test1.pluginhound.com --keep-until-expiring
+# fi
 
-certbot renew --dry-run
+# certbot renew --dry-run
 
 nginx -g 'daemon off;'
