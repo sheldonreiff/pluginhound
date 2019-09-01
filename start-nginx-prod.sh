1a6838308abd4fa5d8b9a00fc6ae7ec18fa66276
Sheldon <sheldonreiff@gmail.com>
2019-08-31 22:05:10 -0400

    Update start-nginx-prod.sh

--- start-nginx-prod.sh
+++ start-nginx-prod.sh
@@ -3,16 +3,16 @@ set -xe
 
 if [ "$APP_ENV" = "test" ];
 then
-	cp /opt/.nginx/conf-test.d/ /etc/nginx/conf.d/
+	cp -r /opt/.nginx/conf-test.d/ /etc/nginx/conf.d/
 else
-	cp /opt/.nginx/conf-prod.d/ /etc/nginx/conf.d/
+	cp -r /opt/.nginx/conf-prod.d/ /etc/nginx/conf.d/
 fi
 
 nginx
 
 if [ ! -d /etc/letsencrypt/live/www.$DOMAIN ];
 then
-	certbot --debug --non-interactive --agree-tos --debug --nginx --email $SSL_ACCOUNT_EMAIL --domains $DOMAIN,www.$DOMAIN --keep-until-expiring
+	certbot --debug --non-interactive --agree-tos --debug --nginx --email $SSL_ACCOUNT_EMAIL --domains $DOMAIN,www.$DOMAIN --keep-until-expiring --redirect
 fi
 
 certbot renew --dry-run
