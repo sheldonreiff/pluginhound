e9542c05ecd1a5989599c7bb91eafdbf185cfc44
Sheldon <sheldonreiff@gmail.com>
2019-09-02 17:12:34 -0400

    Update ImportProducts.php

--- app/Jobs/ImportProducts.php
+++ app/Jobs/ImportProducts.php
@@ -8,6 +8,8 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
+use App\Product;
+
 class ImportProducts implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
