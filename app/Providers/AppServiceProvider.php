3052e2f8034228e6863b1558bb4340561834ee60
Sheldon <sheldonreiff@gmail.com>
2019-12-06 15:26:05 -0500

    report failed jobs to sentry

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Queue\Events\JobFailed;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +27,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(base_path(), 'root');
+
+        Queue::failing(function (JobFailed $event) {
+            app('sentry')->captureException($event->exception);
+        });
     }
 }
